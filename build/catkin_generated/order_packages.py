8578f2a3b75f1d7ae3117f1ce231de38fb782f73
Hussain004 <hussain04habib@gmail.com>
2024-08-05 15:46:56 +0500

    fix

--- build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/home/il_predestinato/multiagent_ws/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/il_predestinato/Hussain_ws/devel;/home/il_predestinato/catkin_ws/devel;/opt/ros/melodic'.split(';') if '/home/il_predestinato/Hussain_ws/devel;/home/il_predestinato/catkin_ws/devel;/opt/ros/melodic' != '' else []
--- build/robomaster_interface/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/il_predestinato/multiagent_ws/src/robomaster_interface
-# Build directory: /home/il_predestinato/multiagent_ws/build/robomaster_interface
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
